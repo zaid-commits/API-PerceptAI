c650c451d6c547bc7aaea311d61c85eeea12f85b
Zaid Rakhange <zaidcommits.github@gmail.com>
2025-02-24 13:06:39 +0530

    upd :: api fetch

--- src/app.py
+++ src/app.py
@@ -38,4 +38,4 @@ def run_project(project_name):
 
 if __name__ == '__main__':
     # Run the application
-    app.run(host='0.0.0.0', port=5000)  # Change to your desired host and port
+    app.run(host='0.0.0.0', port=5050)  # Change to your desired host and port
--- src/projects/project13/main.py
+++ src/projects/project13/main.py
@@ -19,7 +19,7 @@ with col2:
     output_text_area = st.subheader("")
  
  
-genai.configure(api_key="your api key")
+genai.configure(api_key="")
 model = genai.GenerativeModel('gemini-1.5-flash')
  
 # Initialize the webcam to capture video
